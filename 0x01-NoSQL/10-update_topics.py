aa47e0a490ac7dc4cb2d1a5188195557c0f128a5
Sibongile <sibongile.nhlema@gmail.com>
2024-06-18 04:11:47 -0700

    added error handling

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -12,4 +12,14 @@ def update_topics(mongo_collection, name, topics):
         name (str): school name to update
         topics (list of str): list of topics approached in the school
     '''
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    # Check if the document exists
+    existing_school = mongo_collection.find_one({"name": name})
+    if existing_school:
+        # Update topics for the found document
+        mongo_collection.update_one(
+            {"name": name},
+            {"$set": {"topics": topics}}
+        )
+    else:
+        # Handle case where document with 'name' does not exist
+        print(f"Document with name '{name}' not found. Skipping update.")

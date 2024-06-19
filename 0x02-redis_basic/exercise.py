a1a995ace8404f1bdc38af7359c654cb6e7fa92b
Sibongile <sibongile.nhlema@gmail.com>
2024-06-19 20:42:27 +0000

    completd task one and amended the pycodestyle issues

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,17 +4,47 @@ Module handling the method "store" that generates a random key
 '''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     def __init__(self):
-        """Initialize the Redis client and flush the database."""
+        '''
+        Initialize the Redis client and flush the database.
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store data in Redis with a random key and return the key."""
+        '''
+        Store data in Redis with a random key and return the key.
+        '''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes,
+                                                    int, float, None]:
+        '''
+        Retrieve data from Redis and apply an
+        optional callable to convert the data.
+        '''
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''
+        Retrieve data from Redis and convert it to a string.
+        '''
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        '''
+        Retrieve data from Redis and convert it to an integer.
+        '''
+        return self.get(key, fn=int)
